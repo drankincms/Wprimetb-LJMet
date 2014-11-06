21b70d177430a3522beb84cb31340b363c170e74
Dylan <drankin@bu.edu>
2014-11-05 21:09:53 -0600

    fixing for miniAOD and 720

--- Com/python/dbsql_test.py
+++
@@ -1,9 +0,0 @@
-#!//usr/bin/python
-
-import subprocess
-
-dbsql_query = '\'find dataset where dataset like /TTbar/Summer10-START36_V9_S09-v1/GEN-SIM-RECO\''
-
-dbsql = subprocess.Popen(['dbsql', 'find', 'dataset'], stdout=subprocess.PIPE).communicate()[0]
-
-print dbsql
